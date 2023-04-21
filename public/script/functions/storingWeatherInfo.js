ce9931f9f88ff9e3812dd8dcfe5e3315b849b116
Costin Lotreanu <84856918+costinlotreanu@users.noreply.github.com>
2023-04-21 20:03:47 +0300

    Delete storingWeatherInfo.js

--- public/script/functions/storingWeatherInfo.js
+++
@@ -1,9 +0,0 @@
-//storing value from Weather API in data for sending to Database on server
-import { data } from '../index.js';
-
-export function storingWeatherInfo() {
-  data.temperature = temperature;
-  data.status = text;
-  data.windspeed = windspeed;
-  data.winddirection = winddirection;
-}
